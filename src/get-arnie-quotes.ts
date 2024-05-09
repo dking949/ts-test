61bec698b1d32998d5718125d121b4188dfeb1c1
darren king <dking949@gmail.com>
2024-05-09 13:27:38 +1000

    Changes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,11 +1,32 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+type TResult = {
+  "Arnie Quote"?: string;
+  FAILURE?: string;
+};
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  // TEST
-  
-  return [];
+type TApiResponse = {
+  status: number;
+  body: string;
+};
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const requests = urls.map((url) => httpGet(url));
+  const responses: TApiResponse[] = await Promise.all(requests);
+
+  const arnieResults: TResult[] = responses.map((resp) => {
+    const respBody = JSON.parse(resp.body);
+
+    if (resp.status === 200) {
+      return {
+        "Arnie Quote": respBody.message,
+      };
+    } else {
+      return {
+        FAILURE: respBody.message,
+      };
+    }
+  });
+
+  return arnieResults;
 };
